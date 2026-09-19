3d43d183bbcb2d6a1257ebebd0c75351e7f3df22
Mirko Mento <mirko.mento@edu-its.it>
2026-09-19 08:50:53 +0000

    fifth excersise

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,8 +1,18 @@
+import { useEffect, useRef } from "react"
+
+
 function SearchBar({ searchTerm, onSearch }) {
+
+  const inputRef = useRef(null)
+
+  useEffect(() => {
+    inputRef.current?.focus();
+  }, []);
 
   return (
     <input className="search-bar"
       type="text" 
+      ref={inputRef}
       value={searchTerm} 
       onChange={(e) => onSearch(e.target.value)}
       placeholder='Cerca film ...'>
